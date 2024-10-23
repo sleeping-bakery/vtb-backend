cd113dc1540f989029b7c8ff648af35b58db78d1
Vadim Shabanov <Vadim.Shabanov@nedra.digital>
2024-10-23 16:07:15 +0300

    Add docker compose

--- Multibanking.Infrustructure/ServiceCollectionExtensions/InfrastructureServiceCollectionExtensions.cs
+++ Multibanking.Infrustructure/ServiceCollectionExtensions/InfrastructureServiceCollectionExtensions.cs
@@ -8,6 +8,6 @@ public static class InfrastructureServiceCollectionExtensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection serviceCollection)
     {
         // TODO: Add connection string param
-        return serviceCollection.AddDbContext<MultibankingDbContext>(options => options.UseNpgsql("Server=127.0.0.1;Port=5432;Database=postgres;User Id=postgres;Password=postgres;"));
+        return serviceCollection.AddDbContext<MultibankingDbContext>(options => options.UseNpgsql("Server=postgres;Port=5432;Database=postgres;User Id=postgres;Password=postgres;"));
     }
 }
