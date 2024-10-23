f3c155d7a26895425b8c69e35dd03addbfaa112c
Vadim Shabanov <Vadim.Shabanov@nedra.digital>
2024-10-23 17:34:11 +0300

    Put envs to IConfiguration

--- Multibanking.Infrustructure/ServiceCollectionExtensions/InfrastructureServiceCollectionExtensions.cs
+++ Multibanking.Infrustructure/ServiceCollectionExtensions/InfrastructureServiceCollectionExtensions.cs
@@ -8,6 +8,36 @@ public static class InfrastructureServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection serviceCollection, IConfiguration configuration)
     {
+        configuration.SubstituteEnvironmentVariables();
         return serviceCollection.AddDbContext<MultibankingDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+    }
+    
+    private  static void SubstituteEnvironmentVariables(this IConfiguration configuration)
+    {
+        var keys = configuration.AsEnumerable().Select(section => section.Key).ToList();
+
+        foreach (var key in keys)
+        {
+            var value = configuration[key];
+            if (value == null) continue;
+            var substitutedValue = SubstituteEnvironmentVariables(value);
+            if (configuration is IConfigurationRoot configRoot)
+            {
+                configRoot[key] = substitutedValue;
+            }
+        }
+    }
+    private static string SubstituteEnvironmentVariables(string str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return str;
+
+        // Используем регулярное выражение для поиска всех ${VARIABLE_NAME}
+        var regex = new System.Text.RegularExpressions.Regex(@"\${(.*?)}");
+        return regex.Replace(str, match =>
+        {
+            var envVariable = match.Groups[1].Value; // Получаем имя переменной
+            return Environment.GetEnvironmentVariable(envVariable) ?? match.Value; // Подставляем значение или оставляем как есть
+        });
     }
 }
