1f0a0fa8a000764a0149fb3b6855a43a0015a56b
Vadim Shabanov <Vadim.Shabanov@nedra.digital>
2024-10-23 16:50:34 +0300

    Fix auth

--- Multibanking.API/Program.cs
+++ Multibanking.API/Program.cs
@@ -44,7 +44,7 @@ public class Program
                     // TODO: Прокидывать через конфиги
                     ValidIssuer = "http://localhost:8080/realms/master",
                     // TODO: Поменять на true, когда будем хоститься
-                    ValidateLifetime = false,
+                    ValidateLifetime = true,
 
                     ValidateSignatureLast = true,
                     ValidateTokenReplay = true,
@@ -53,10 +53,47 @@ public class Program
                     ValidateIssuerSigningKey = true,
                     // TODO: получить напрямую с КК
                     IssuerSigningKey = new JsonWebKey(
-                        @"{""keys"":[{""kid"":""x8iUtT5g62gjlSfsOP1ETapFlwO7-VnQXbxh3zWkGyw"",""kty"":""RSA"",""alg"":""RS256"",""use"":""sig"",""n"":""twG0FyOlpnGN_Ch7lHCJYPFPBkHQlHFTX29Li2uXzKaWRFFxHdkbIRbkux--J9HPkrefv0kXOJBVqEDTX3xuDhhFifU8blyn1o53BILbTG25cEUkUWb9jZqo9NDmgatiu44AmDVS-IrFZyqLT_7OyOFpzuhDb702Cd5qkfu4buIwPf1P26ifFFGo1VnpRIxdRhbiQ34_fWwR2thxng_HCwBxg2eLE_OlWtoBo3PWKJu3j1uOYCEUMWwuwkVCJhOaqC-j-27_3utF6ryj9TVdqP3mJurJNlBoA1aNE2iQV0QqlkYxBbpFtjPaDDTS5T88HL8ZwU5w-lX6rIB3A0AklQ"",""e"":""AQAB"",""x5c"":[""MIICmzCCAYMCBgGStgfs5DANBgkqhkiG9w0BAQsFADARMQ8wDQYDVQQDDAZtYXN0ZXIwHhcNMjQxMDIyMjA1ODQ1WhcNMzQxMDIyMjEwMDI1WjARMQ8wDQYDVQQDDAZtYXN0ZXIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC3AbQXI6WmcY38KHuUcIlg8U8GQdCUcVNfb0uLa5fMppZEUXEd2RshFuS7H74n0c+St5+/SRc4kFWoQNNffG4OGEWJ9TxuXKfWjncEgttMbblwRSRRZv2Nmqj00OaBq2K7jgCYNVL4isVnKotP/s7I4WnO6ENvvTYJ3mqR+7hu4jA9/U/bqJ8UUajVWelEjF1GFuJDfj99bBHa2HGeD8cLAHGDZ4sT86Va2gGjc9Yom7ePW45gIRQxbC7CRUImE5qoL6P7bv/e60XqvKP1NV2o/eYm6sk2UGgDVo0TaJBXRCqWRjEFukW2M9oMNNLlPzwcvxnBTnD6VfqsgHcDQCSVAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAFVk6vsj0nw070hVFjaZ25hUDpC8bOSM3p3Q7o6H8iYa7nz5qPM3N0xe6LzG4AaL4DJM+70VEAF2sgkM6OS0IVW+T0dVscQTLKowEebm7okqvoy3t/z4vYwFgOGXh+KTa9tbog0hlOZDsZo4mJbt2i7QynKMyNwPGfbjk1yAw/g27xlKADEPyNCXkC6xMx8hMzMAcKnlwkdT8q+inesGzgCrUvfBtc9YnhoiMpZr7kqtb8b3LsW/h+RrhaGD+gFuX+6XrfswLkStaTfHLMJl5HxRSYE92mfG+f3e2R2g2yzMz7/vrPtTTs/76CcoYDXI/24KMsTD+5FIFC6Bc7weJs8=""],""x5t"":""66AgaXV_goAJfXLwpqLz4EDB9Yc"",""x5t#S256"":""EBWp5-QvtvhhnEZ0k4ypcYfod8kxz1yUMe0duopSnfM""},{""kid"":""d_9NCTcCaWnvtjo9zQNBz4NC1drKcGR0EjXrMPI1dE0"",""kty"":""RSA"",""alg"":""RSA-OAEP"",""use"":""enc"",""n"":""11xfjH8ASepFt7aiTO8yGPg-jbiDwUtXVpHBvXVIECIU7-pXQdKqItQvtvkVfacNcguDn-jgTRpYdLv3QWCIua8MgahaNIlGffn4b1jFMDIKOGQ-enD7marJpDFcUTesXvVWfdl5zUQVZVQRW4JsPeUxYMAI83Us3f0f5T5JKuzVUTUOcU5m1behJk3tn4knMuPuqW84GhpAf1I8r-XkgT1TmJQ8Mg-GSClgvV0kmYmNvHpNR5LGYYfQdVdjkjOFoJYwjRpL40KaXe5AGqmIOcxRE_aPi_KwT0t6o7mycndQ1Q8HGXGkQ5M-xx96wyHO23jgJotSHDs_qDfMPIRnuw"",""e"":""AQAB"",""x5c"":[""MIICmzCCAYMCBgGStgftdzANBgkqhkiG9w0BAQsFADARMQ8wDQYDVQQDDAZtYXN0ZXIwHhcNMjQxMDIyMjA1ODQ1WhcNMzQxMDIyMjEwMDI1WjARMQ8wDQYDVQQDDAZtYXN0ZXIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDXXF+MfwBJ6kW3tqJM7zIY+D6NuIPBS1dWkcG9dUgQIhTv6ldB0qoi1C+2+RV9pw1yC4Of6OBNGlh0u/dBYIi5rwyBqFo0iUZ9+fhvWMUwMgo4ZD56cPuZqsmkMVxRN6xe9VZ92XnNRBVlVBFbgmw95TFgwAjzdSzd/R/lPkkq7NVRNQ5xTmbVt6EmTe2fiScy4+6pbzgaGkB/Ujyv5eSBPVOYlDwyD4ZIKWC9XSSZiY28ek1HksZhh9B1V2OSM4WgljCNGkvjQppd7kAaqYg5zFET9o+L8rBPS3qjubJyd1DVDwcZcaRDkz7HH3rDIc7beOAmi1IcOz+oN8w8hGe7AgMBAAEwDQYJKoZIhvcNAQELBQADggEBAAmvzQoausEpzykFkZDFsj+SXVRrhTyW5e32RDN6Tkc+xik/LKfxw5vOT4YwduP8XiF00610bvSF1OlB8OM4A8b/Ya8pFCspkc1yKQtTb9a2IrWOwDgT8LV7IAs0WmNhys+UQrpM/41pv0MyipqcBDUDpbkKlFrRaQhxbKu0k3pjx73rjbRFHOf7TvWbUv//AxDCLHfwccHzfY7ZgoA7gOAwaSWcw6RpI6qpa7puCNqQl735GxMwKz2sqxmbaPCRKOvdcgZOQG0yCuvEAQzVCYsyTWyj/9WRpOOoiTqxxgv/G8QK2O+zkeZboLsV27kxK8Gc6TA5Ssq5mJBnbnhTi7A=""],""x5t"":""JlrdftAx9bP0KGpbnlK8kHL2lxw"",""x5t#S256"":""iI9-iEiozD5ZGYLCZP7c3p3bAW_kP97L2A-JBL38ujM""}]}"),
+                        @"{
+    ""keys"": [
+        {
+            ""kid"": ""FINVMCN_8UMTQmhMLkYmtkfKgH3cerITmlgqQs9RECI"",
+            ""kty"": ""RSA"",
+            ""alg"": ""RS256"",
+            ""use"": ""sig"",
+            ""n"": ""sgOG-qAtqIol3BXzDdS6WUBh4_g4s5Ua70epvIgswwqMR2NUVcvo4pWdlrBh600mfdmKDHxYv5257WT5DLvhCSu4HNdk9JlhsiLV3sGVAXoNvULpz2uvaukyD3k_31RyKDo4hgMXmwB6vpMKI9a2ftUOV1wFgfBRKrrnzwuDRN_Pp0L_KpLePhHYluDQhmkRGrUjocU1BY70k8PIGOI1IfRwzqMqnXz7CFYR_i0HVAfw0mget94t300FJMYffXbMfIvbu4Axr5AMbEYsjPdIKQduanAj4HH5NKKVpO4QtKycYnNAMqMlQMrS5_8fHvhhm66PWyF5DyyZvA1VWCxhVQ"",
+            ""e"": ""AQAB"",
+            ""x5c"": [
+                ""MIICmzCCAYMCBgGSuZTR1TANBgkqhkiG9w0BAQsFADARMQ8wDQYDVQQDDAZtYXN0ZXIwHhcNMjQxMDIzMTMzMTMxWhcNMzQxMDIzMTMzMzExWjARMQ8wDQYDVQQDDAZtYXN0ZXIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCyA4b6oC2oiiXcFfMN1LpZQGHj+DizlRrvR6m8iCzDCoxHY1RVy+jilZ2WsGHrTSZ92YoMfFi/nbntZPkMu+EJK7gc12T0mWGyItXewZUBeg29QunPa69q6TIPeT/fVHIoOjiGAxebAHq+kwoj1rZ+1Q5XXAWB8FEquufPC4NE38+nQv8qkt4+EdiW4NCGaREatSOhxTUFjvSTw8gY4jUh9HDOoyqdfPsIVhH+LQdUB/DSaB633i3fTQUkxh99dsx8i9u7gDGvkAxsRiyM90gpB25qcCPgcfk0opWk7hC0rJxic0AyoyVAytLn/x8e+GGbro9bIXkPLJm8DVVYLGFVAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAIKjQe/wVUR6bt54GuqvtA6ywlmmhsCzIFiZB0nemZt3Bw29ebj42RBdy87rnLcsfI4AT3yCaKhVrGJ0kC3LJ1B3XHMfWr7OVTW8HINlfkmO6iOWKILhl5GufPM+mjvJ52I/MKE579wnep+84EO3tpziBQR7fKY3tMGVHG04KRh1NLgC5GsNadyQIhy+814S+EwDBHCrTUD2i4HV8nub1fsGHTKjgYF28O5TPU3jHRjNdVV6YY3tA+8HmjlPUmb4558jz03x7FbH1AEDUUwd4lRzXwsfqoNzpFGKwYcYtuQTmMK31YIdWV6iTbnsQSFd674/zaONnynyAjEoCdcugL0=""
+            ],
+            ""x5t"": ""zo0ZBEslNfF4kHRO9bWv2J1CbuU"",
+            ""x5t#S256"": ""Iq9l2mStZo6KRQLIBPCm-BLOCv6Rx8t11mooR61lNeY""
+        },
+        {
+            ""kid"": ""ENZbly2pYWZbqNaAPTKQSuhgcAxD5sY3Su2HrUQ590k"",
+            ""kty"": ""RSA"",
+            ""alg"": ""RSA-OAEP"",
+            ""use"": ""enc"",
+            ""n"": ""39_qhlMwwNoNpMDIwdo3UeXPqAUpuMJrWzSOwfKLjDEb2Ti2nmLcgu-EhNqS_2lAf8sPjSwTOF8QRCFSrGnfVpfQZFFMd2FopB9oIJcrYEMMuCNzC87LNdX1E7LnsZLwH9b8XZJRKxRqFcDWZgww3bSA_DYJawM7Dob9e1y9sGK5htDwPBWlONEpmPW89Z2o9-GMzh7K1wY4aLNGt7Idj9NVoZQwQ6F30wMGoWIhk5EzQkYcqZ6x0RKgO5bJ-RJQCuk_FcYoIE-_6iYBFDBYtw25RHoZcg_4amu5S9XZLKtI82T4f-Wkdaj5xENxcN8Jr_TBcWL6W5LG_R2RFyLlGw"",
+            ""e"": ""AQAB"",
+            ""x5c"": [
+                ""MIICmzCCAYMCBgGSuZTShTANBgkqhkiG9w0BAQsFADARMQ8wDQYDVQQDDAZtYXN0ZXIwHhcNMjQxMDIzMTMzMTMxWhcNMzQxMDIzMTMzMzExWjARMQ8wDQYDVQQDDAZtYXN0ZXIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDf3+qGUzDA2g2kwMjB2jdR5c+oBSm4wmtbNI7B8ouMMRvZOLaeYtyC74SE2pL/aUB/yw+NLBM4XxBEIVKsad9Wl9BkUUx3YWikH2gglytgQwy4I3MLzss11fUTsuexkvAf1vxdklErFGoVwNZmDDDdtID8NglrAzsOhv17XL2wYrmG0PA8FaU40SmY9bz1naj34YzOHsrXBjhos0a3sh2P01WhlDBDoXfTAwahYiGTkTNCRhypnrHREqA7lsn5ElAK6T8VxiggT7/qJgEUMFi3DblEehlyD/hqa7lL1dksq0jzZPh/5aR1qPnEQ3Fw3wmv9MFxYvpbksb9HZEXIuUbAgMBAAEwDQYJKoZIhvcNAQELBQADggEBANI/A6WszvtWaq5ZZkw5UtNiiw9+9nJW72HqF1QPJFFJLncT3o36cq7X1qQCnGUy1aerC2K3L4+zQfLVB7G0ZSt/iLhhsGEbndZ7L737j3tssNOtfuwoLlZuF1T/DBV97VnIZ1rFmjR9Ch6iQ/0KXDPX89L4WCCJmqO1BC4il6ltH0sX4lq8MrdUTK92yD4NGqVt5f/E4sVkCICO/KibOQVsIOYFov3Q2RxDTqOKyTdx7AfB8gGHVjwfSH7YFed9PvgFvn3l07bZf5fJ/n0Yy4V2tIiya7A1J9ScaaHYjMOg0073+G+CbQElijEezKnknqYHYze/64aNWhGVgvxzQF4=""
+            ],
+            ""x5t"": ""6SF2zgV3Iprf0YJBmqlMFxdGYPU"",
+            ""x5t#S256"": ""vgWKMilRDDO31PO2VWD6X5FsA_zV58sMxYQPpO3T2fA""
+        }
+    ]
+}"),
                     LogValidationExceptions = true,
+                    
                 };
-
+                options.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = c =>
+                    {
+                        Console.WriteLine(c.Exception.Message);
+                        return Task.CompletedTask; 
+                    }
+                };
                 // TODO: Убрать когда захостимся, оставить только для дебага
                 options.RequireHttpsMetadata = false;
             });
