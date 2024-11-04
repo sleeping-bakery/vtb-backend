f475ff0f98405e256c435fc2ccc9ce3706df97ae
Vadim Shabanov <Vadim.Shabanov@nedra.digital>
2024-11-04 05:52:38 +0300

    Test VSC

--- Multibanking.API/Controllers/AccountConsentController.cs
+++ Multibanking.API/Controllers/AccountConsentController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Multibanking.Services.Account;
 
 namespace Multibanking.API.Controllers;
-
+ 
 [ApiController]
 [Route("[controller]")]
 public class AccountConsentController(IAccountConsentsService accountConsentsService) : ControllerBase
